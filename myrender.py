6a0374cfccd885f33be9de5e075780e25a5b22d3
y-shoji <p108053y@mail.kyutech.jp>
2019-08-28 13:33:20 +0900

    detail.html add json path

--- myrender.py
+++ myrender.py
@@ -1,9 +1,13 @@
 import json
 
-def craft(title):
-    with open('./static/json/craft.json', 'r') as f:
+def read_json(title,path):
+    with open(path, 'r') as f:
         json_dict = json.load(f)
     anime=[]
     anime.append(json_dict[title])
     return anime
 
+def read_json2(title,path):
+    with open(path, 'r') as f:
+        json_dict = json.load(f)
+    return json_dict[title]
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
-from myrender import craft
+from myrender import read_json,read_json2
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
@@ -10,11 +10,13 @@ def hello_world():
 
 @app.route('/detail')
 def detail():
-    return render_template('detail-experience.html')
+    title = "法被の試着体験会"
+    details = read_json2(title,"./static/json/detail.json")
+    return render_template('detail-experience.html',details= details,title=title)
 
 @app.route('/craft/<title>')
 def craft_view(title):
-    anime = craft(title)
+    anime = read_json(title,"./static/json/craft.json")
     return render_template('craft.html',anime=anime,title=title)
 
 if __name__ == '__main__':
