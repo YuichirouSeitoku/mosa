b49bc428f728a74c603c1eb9126e230a20da1334
AsaiTomohiro <benefit20002@gmail.com>
2019-08-28 13:56:57 +0900

    add image

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('template.html', my_string="Hello world", my_list=[0,1,2,3,4,5])
+    return render_template('top.html')
 
 
 
