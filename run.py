7b0b1f1415bb96e4e107fc3907d44bb8aa6542e5
y-shoji <p108053y@mail.kyutech.jp>
2019-08-28 18:01:58 +0900

    update top2

--- run.py
+++ run.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 @app.route('/')
-def hello_world():
-    return render_template('top2.html')
+def top_page():
+    anime_link = read_json2("img_link","./static/json/anime.json")
+    return render_template('top2.html',anime_link=anime_link)
 
 @app.route('/detail')
 def detail():
@@ -14,9 +15,8 @@ def detail():
     details = read_json2(title,"./static/json/detail.json")
     return render_template('detail-experience.html',details= details,title=title)
 
-@app.route('/craft')
-def craft_view():
-    title = "ハナヤマタ"
+@app.route('/craft/<title>')
+def craft_view(title):
     anime = read_json(title,"./static/json/craft.json")
     return render_template('craft.html',anime=anime,title=title)
 
